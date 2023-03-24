8c3f5adffcc842ae2aaad2f19a4214a3b37d682d
Kirill <64438506+qouglou@users.noreply.github.com>
2023-03-24 15:11:05 +0300

    Update DataBase.cs

--- SilaGor/DataBase.cs
+++ SilaGor/DataBase.cs
@@ -9,7 +9,7 @@ namespace SilaGor
 {
     class DataBase
     {
-        MySqlConnection connection = new MySqlConnection("server=a0423736.xsph.ru;port=3306;username=a0423736;password=avtiibamze;database=a0423736_silagor");
+        MySqlConnection connection = new MySqlConnection("server=server_name;port=3306;username=user_name;password=password;database=db_name");
 
         public void openConnection()
         {
